cbdf31084ce6e04a8a6d3ce91484f1e60acb79db
Maura Hunter <Maura Hunter@192.168.21.71>
2023-09-11 11:58:09 -0400

    I changed the sphere radius, vertical divison and elevation

--- sphere.groovy
+++ sphere.groovy
@@ -1,10 +1,10 @@
 //create a sphere
-CSG sphere = new Sphere(25)// Spheres radius
+CSG sphere = new Sphere(80)// Spheres radius
 				.toCSG()// convert to CSG to display
 				
-CSG sphereHighRes = new Sphere(25,// Spheres radius
+CSG sphereHighRes = new Sphere(120,// Spheres radius
 						40,//elevation Divisions
-						40)// vertical divisions
+						100)// vertical divisions
 						.toCSG()// convert to CSG to display
 sphere=sphere.movex(55)
 return [sphere,sphereHighRes]
